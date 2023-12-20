344ce876e0dcff019baf6ff0b3341c3a73ca9c59
Asma-aics50 <114606316+Asma-aics50@users.noreply.github.com>
2023-12-20 15:44:31 +0200

    Add files via upload

---
+++ Task4.1/Task4.1/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Task4._1
+{
+	public class Account
+	{
+		private decimal balance;
+		private string name;
+
+		public Account()
+		{
+			Name = null;
+			balance = 0;
+		} public Account(string name, decimal balance) {
+
+			this.Name = name;
+			this.balance = balance;
+
+
+		}
+
+		public bool Deposit(decimal amountAdd) {
+
+			if (amountAdd > 0) {
+
+                balance += amountAdd;
+				return true;
+
+            }
+			return false;
+
+		}
+		public bool Withdraw(decimal amountSubtract) {
+
+
+            if (amountSubtract > 0 && amountSubtract <= balance)
+            {
+
+                balance -= amountSubtract;
+                return true;
+
+            }
+            return false;
+
+        }
+		public string Name { get; }
+
+		public void Print() {
+
+			Console.WriteLine($"name is {Name}  and your balance is {balance}");
+			
+
+        }
+
+    }
+}
+
---
+++ Task4.1/Task4.1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Task4._1;
+
+public enum Menuoption
+{
+    Withdraw =0,
+    Deposit=1,
+    Print=2,
+    Quit=3
+
+}
+
+
+class Program
+{
+    
+    public static Menuoption ReadUseroption()
+    {
+        int num = '\0';
+
+        do {
+            Console.WriteLine("choise from my list ");
+            Console.WriteLine("0 - Withdraw");
+            Console.WriteLine("1 - Deposite");
+            Console.WriteLine("2 - Print");
+            Console.WriteLine("3 - Quit");
+
+            Console.WriteLine("enter your choise number :");
+            num = Convert.ToInt32(Console.ReadLine());
+
+
+        } while (num < 0 && num > 3 );
+      return (Menuoption)num;
+       
+    }
+    private static void Dowithdraw(Account account1)
+    {
+        Decimal num = '\0';
+        Console.WriteLine("Enter the Withdraw amount :");
+        num = Convert.ToDecimal(Console.ReadLine());
+
+        account1.Withdraw(num);
+
+    }
+    private static void DoDeposit(Account account1)
+    {
+        Decimal num = '\0';
+        Console.WriteLine("Enter the deposit amount :");
+        num = Convert.ToDecimal(Console.ReadLine());
+
+        account1.Deposit(num);
+
+
+    }
+    public static void DoPrint(Account account1)
+    {
+
+        account1.Print();
+
+    }
+
+
+
+
+    static void Main(string[] args)
+    {
+        Account account = new Account("Jakes", 200000);
+        account.Print();
+        //account.Deposit(100);
+        //account.Print();
+        //account.Withdraw(200);
+        //account.Print();
+
+        Account account1 = new Account("Asmaa",1000);
+        //account1.Deposit(1000);
+        account1.Print();
+        //account.Withdraw(500);
+        //account1.Print();
+        do {
+
+            Menuoption selectedOption = ReadUseroption();
+
+            switch (selectedOption)
+            {
+                case Menuoption.Withdraw:
+                    Dowithdraw(account1);
+                    break;
+                case Menuoption.Deposit:
+                    DoDeposit(account1);
+                    break;
+                case Menuoption.Print:
+                    DoPrint(account1);
+                    break;
+                case Menuoption.Quit:
+                    Console.WriteLine($"{Menuoption.Quit}");
+                    break;
+                default:
+                    Console.WriteLine("invalid option");
+
+                    break;
+
+            }
+
+
+        } while (true);
+        
+
+    }
+    
+}
+
